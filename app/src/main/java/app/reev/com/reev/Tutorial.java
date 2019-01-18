1c7d0ea416c4459fd5b8fa50f5d3c3dad99fbffe
marktoBR <nocettimarcos@gmail.com>
2019-01-17 23:24:53 -0200

    login add

--- app/src/main/java/app/reev/com/reev/Tutorial.java
+++ app/src/main/java/app/reev/com/reev/Tutorial.java
@@ -1,5 +1,6 @@
 package app.reev.com.reev;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Tutorial extends AppCompatActivity {
@@ -56,7 +58,9 @@ public class Tutorial extends AppCompatActivity {
 
 
 
+
     }
+
 
 
     @Override
@@ -80,6 +84,12 @@ public class Tutorial extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void proximaTela(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
 
     /**
      * A placeholder fragment containing a simple view.
