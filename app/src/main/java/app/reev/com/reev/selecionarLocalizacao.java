dac9f7b6cf59cbd5fe326a871da5e453ac370ec2
BulbaStitch <lucas.peres16@hotmail.com>
2019-01-21 00:16:29 -0200

    f
    f

--- app/src/main/java/app/reev/com/reev/selecionarLocalizacao.java
+++ app/src/main/java/app/reev/com/reev/selecionarLocalizacao.java
@@ -14,6 +14,7 @@ public class selecionarLocalizacao extends FragmentActivity implements OnMapRead
 
     private GoogleMap mMap;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
